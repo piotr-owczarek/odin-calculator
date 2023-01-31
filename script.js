5e159b3c864b2fd11617eb8ab5fcd017e4c5e82c
Piotr Owczarek <piotrek.zwd@gmail.com>
2023-01-31 22:30:29 +0100

    Fixed logic

--- script.js
+++ script.js
@@ -4,20 +4,24 @@ const clearButton = document.getElementById('btn-clear');
 const currentScreen = document.querySelector('.screen-now');
 const lastScreen = document.querySelector('.screen-last');
 const equalButton = document.querySelector('.btn-equal');
-let lastValue;
-let usedOperator;
+let lastValue = '';
+let usedOperator = '';
 let shouldResetScreen = false;
+let operatorClicked = false;
 
 numberButtons.forEach(btn => {
-    btn.addEventListener('click', () => appendCurrentScreen(btn.textContent))
+    btn.addEventListener('click', () => {
+        appendCurrentScreen(btn.textContent);
+        operatorClicked = false;
+    });
 });
 
 operatorButtons.forEach(btn => {
-    btn.addEventListener('click', () => currentValue(currentScreen.textContent, btn.textContent))
+    btn.addEventListener('click', () => currentValue(currentScreen.textContent, btn.textContent)) 
 });
 
 clearButton.addEventListener('click', () => clear());
-equalButton.addEventListener('click', () => operate(usedOperator, lastValue, currentScreen.textContent))
+equalButton.addEventListener('click', () => equalSign(usedOperator, lastValue, currentScreen.textContent))
 
 function appendCurrentScreen(number) {
     if (currentScreen.textContent == 0) currentScreen.textContent = '';
@@ -29,18 +33,40 @@ function appendCurrentScreen(number) {
 function clear() {
     currentScreen.textContent = 0;
     lastScreen.textContent = '';
+    lastValue = '';
+    usedOperator = '';
 }
 
 function currentValue(value, operator) {
     lastScreen.textContent = value + ' ' + operator;
+
+    if (!operatorClicked) {
+        if (usedOperator !== '' && lastValue !== '') {
+            if (usedOperator == operator) {
+                operate(operator, lastValue, value);
+            }
+            else {
+                operate(usedOperator, lastValue, value);
+            }
+            lastScreen.textContent = currentScreen.textContent + ' ' + operator;
+            value = currentScreen.textContent;   
+        }
+    }
+
+    operatorClicked = true;
     lastValue = value;
     usedOperator = operator;
     shouldResetScreen = true;
 }
 
+function equalSign(operator, a, b) {
+    lastScreen.textContent = a + ' ' + operator + ' ' + b + ' =';
+    operate(operator, a, b);
+    usedOperator = '';
+}
 
 function operate(operator, a, b) {
-    if (lastScreen.textContent !== '') lastScreen.textContent += ' ' + b + ' =';
+    lastValue = '';
     switch (operator) {
         case '+':
             return add(a, b);
